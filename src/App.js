49bf31f0c743088386711f08106c62b6ed93af56
HassanGazder <hassan2110c@aptechgdn.net>
2024-01-31 21:59:59 +0500

    some ui design

--- src/App.js
+++ src/App.js
@@ -1,97 +1,168 @@
-import './App.css';
-import { useState } from 'react';
-import img1 from '../src/assets/bilawal.jpg'
-import img2 from '../src/assets/imran-khan-1.jpg'
-import img3 from '../src/assets/naeem.jpg'
-import img4 from '../src/assets/nawaz.jpg'
+import "./App.css";
+import { useState } from "react";
+import img1 from "../src/assets/bilawal.jpg";
+import img2 from "../src/assets/imran-khan-1.jpg";
+import img3 from "../src/assets/naeem.jpg";
+import img4 from "../src/assets/nawaz.jpg";
 
 function App() {
-  let [isdisable,setisdisable] = useState(false)
-  function handledisable(){
-    setisdisable(true)
-    console.log("click")
+  let [isdisable, setisdisable] = useState(false);
+  function handledisable() {
+    setisdisable(true);
+    console.log("click");
   }
   return (
     <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-          <a className="navbar-brand" href="trtr">Navbar</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <a className="navbar-brand" href="trtr">
+            Navbar
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="gg">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="gkjg">Link</a>
-              </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
+                <a className="nav-link active" aria-current="page" href="gg">
+                  Home
                 </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a className="dropdown-item" href="item">Action</a></li>
-                  <li><a className="dropdown-item" href="fhfh">Another action</a></li>
-                  <li><hr className="dropdown-divider" /></li>
-                  <li><a className="dropdown-item" href="hgh">Something else here</a></li>
-                </ul>
               </li>
               <li className="nav-item">
-                <a className="nav-link disabled" href="fhf" tabIndex="-1" aria-disabled="true">Disabled</a>
+                <a className="nav-link active" aria-current="page" href="gg">
+                  About
+                </a>
+              </li> <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="gg">
+                  Contact
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="gg">
+                  Result
+                </a>
               </li>
             </ul>
-            <form className="d-flex">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit">Search</button>
+            <form className="d-flex justify-content-center align-items-center">
+            <i className="fa-regular fa-user"></i>
+              <h5 className="text-white me-3">Hassan Gazder</h5>
+              <button className="btn btn-outline-success" type="submit">Log Out</button>
             </form>
           </div>
         </div>
       </nav>
 
-      <div className='container'>
-        <div className='heading'>
-          <h1>Vote For Your Favourite Candidate</h1>
+      <div className="container">
+        <div className="heading mt-5">
+          <h1 className="fw-bold">Vote For Your Favourite Candidate</h1>
         </div>
-        <div className='row mt-5'>
-          <div className='col-lg-3'>
+        <div className="alert-div d-flex justify-content-center">
+          <div  className={isdisable ? "alert alert-success w-50 d-block" :"alert alert-success w-50 d-none"} role="alert">
+            Your Vote Has Been Submitted Successfully
+          </div>
+        </div>
+        <div className="row mt-4">
+          <div className="col-lg-3">
             <div class="card">
-              <img src={img1} className="card-img-top" alt="..." width={100} height={180} />
+              <img src={img1} className="card-img-top" alt="..." />
               <div class="card-body">
-                <h5 class="card-title">Team One</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <button disabled={isdisable ? true : false} onClick={handledisable} type='submit' href="vote" class="btn btn-primary">Vote</button>
+                <h5 class="card-title">PPP</h5>
+                <p class="card-text">
+                  "I can't let my mother's death have been in vain. Democracy is
+                  the best revenge, and we will have it"
+                </p>
+                <button
+                  disabled={isdisable ? true : false}
+                  onClick={handledisable}
+                  type="submit"
+                  href="vote"
+                  class="btn btn-success"
+                >
+                  Vote
+                </button>
+                <p className="note">
+                  Be Careful Vote Would Be Submitted Only Once
+                </p>
               </div>
             </div>
           </div>
-          <div className='col-lg-3'>
+          <div className="col-lg-3">
             <div class="card">
-              <img src={img4} class="card-img-top" alt="..." width={100} height={180} />
-              <div className="card-body">
-                <h5 className="card-title">Team two</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <button disabled={isdisable ? true : false}  onClick={handledisable} type='submit' href="vote" class="btn btn-primary">Vote</button>
+              <img src={img4} className="card-img-top" alt="..." />
+              <div class="card-body">
+                <h5 class="card-title">PPP</h5>
+                <p class="card-text">
+                  "I can't let my mother's death have been in vain. Democracy is
+                  the best revenge, and we will have it"
+                </p>
+                <button
+                  disabled={isdisable ? true : false}
+                  onClick={handledisable}
+                  type="submit"
+                  href="vote"
+                  class="btn btn-success"
+                >
+                  Vote
+                </button>
+                <p className="note">
+                  Be Careful Vote Would Be Submitted Only Once
+                </p>
               </div>
             </div>
           </div>
-          <div className='col-lg-3'>
+          <div className="col-lg-3">
             <div class="card">
-              <img src={img3} class="card-img-top" alt="..." width={100} height={180} />
+              <img src={img3} class="card-img-top" alt="..." />
               <div className="card-body">
-                <h5 className="card-title">Team Three</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <button disabled={isdisable ? true : false}  onClick={handledisable} type='submit' href="vote" class="btn btn-primary">Vote</button>
+                <h5 className="card-title">Jamatey Islami</h5>
+                <p className="card-text">
+                  "a man in white shirt standing next to a scale with the words
+                  let's rebuild karachi"
+                </p>
+                <button
+                  disabled={isdisable ? true : false}
+                  onClick={handledisable}
+                  type="submit"
+                  href="vote"
+                  class="btn btn-success"
+                >
+                  Vote
+                </button>
+                <p className="note">
+                  Be Careful Vote Would Be Submitted Only Once
+                </p>
               </div>
             </div>
           </div>
-          <div className='col-lg-3'>
+          <div className="col-lg-3">
             <div className="card">
-              <img src={img2} class="card-img-top" alt="..." width={100} height={180} />
+              <img src={img2} class="card-img-top" alt="..." />
               <div className="card-body">
-                <h5 className="card-title">Team Four</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <button disabled={isdisable ? true : false}  onClick={handledisable} type='submit' href="vote" className="btn btn-primary">Vote</button>
+                <h5 className="card-title">PTI</h5>
+                <p className="card-text">
+                  "It is not defeat that destroys you,it is being demoralized by
+                  defeat that destroy you."
+                </p>
+                <button
+                  disabled={isdisable ? true : false}
+                  onClick={handledisable}
+                  type="submit"
+                  href="vote"
+                  className="btn btn-success"
+                >
+                  Vote
+                </button>
+                <p className="note">
+                  Be Careful Vote Would Be Submitted Only Once
+                </p>
               </div>
             </div>
           </div>
